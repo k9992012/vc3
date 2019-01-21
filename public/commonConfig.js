3b65f66e68ed040fecc5167afdd89595aea88594
Administrator <787884981@qq.com>
2019-01-21 17:09:09 +0800

    update

--- public/commonConfig.js
+++ public/commonConfig.js
@@ -1,11 +1,9 @@
-const serverName = 'MonitoringDisplay'
+const serverName = 'MonitoringDisplay' // 后台服务名称
 const path = 'http://10.100.9.43:8081' // 开发环境代理接口
-const mapPath = 'http://10.100.9.168/gdswearth/'
+const mapPath = 'http://10.100.9.168/gdswearth/'// 三维服务接口
 const changeOrigin = false// 开发环境服务是否不同ip
-// 请求刷新时间
-const refreshTime = 1000 * 60 * 5
-// 请求定时器
-let timer = {}
+const refreshTime = 1000 * 60 * 5// 请求刷新时间
+let timer = {}// 请求定时器
 let commonConfig = {
   api: `/${serverName}/`,
   proxy: {},
